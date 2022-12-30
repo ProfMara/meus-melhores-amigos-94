a4a5f1555ea0698053bdd97da915b57dc7565751
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-12-30 09:22:08 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,46 @@
+
+//ADICIONE SEUS LINKS FIREBASE
+const firebaseConfig = {
+    apiKey: "AIzaSyAcR_b4Ts86xAvFApJWvSPi0Y5SElCAKMY",
+    authDomain: "aaaaaaa-cf7fb.firebaseapp.com",
+    databaseURL: "https://aaaaaaa-cf7fb-default-rtdb.firebaseio.com",
+    projectId: "aaaaaaa-cf7fb",
+    storageBucket: "aaaaaaa-cf7fb.appspot.com",
+    messagingSenderId: "359063899711",
+    appId: "1:359063899711:web:5f9c38f945b337cd7ab28a",
+    measurementId: "G-K41ZG98BEW"
+  };
+  
+  
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+  
+function addUser(){
+    //guarda o que o usuário digitou
+
+    //coloca no localStorage
+
+    //troca de site
+
+}
+var campo = '';
+var nome = '';
+function carregar(){
+    //pega o nome do usuário do localStorage
+
+    //exibe na tela
+
+    
+    //lê os dados dentro do banco de dados
+    
+    
+}
+
+function addContact(){
+    //guarda o nome e o número que o usuário digitou
+
+    //escreve no banco de dados
+
+    
+
+}
